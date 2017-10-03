6653590466164be2973c2dc30f9159cd83d0e848
Jack Sea <jack.sidorenko@hotmail.com>
2017-10-02 21:20:19 -0700

    sync

--- drawioDiagrams/StorytellerPlugin/content/js/storyteller_plugin.js
+++ drawioDiagrams/StorytellerPlugin/content/js/storyteller_plugin.js
@@ -80,7 +80,7 @@ Draw.loadPlugin(function(ui)
 	}
 
 	var x = document.createElement('script');
-	x.src = 'https://raw.githubusercontent.com/freewebtime/freewebtime.github.io/master/drawioDiagrams/StorytellerPlugin/content/js/include.js';
+	x.src = 'https://freewebtime.github.io/drawioDiagrams/StorytellerPlugin/content/js/include.js';
 	document.getElementsByTagName("head")[0].appendChild(x);
 	x.onload=callback_function;
 });
