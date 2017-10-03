2de46e0f3ea7cef423e3d20c78158985af2e2d32
Jack Sea <jack.sidorenko@hotmail.com>
2017-10-02 21:45:33 -0700

    asd

--- drawioDiagrams/StorytellerPlugin/content/js/storyteller.core.js
+++ drawioDiagrams/StorytellerPlugin/content/js/storyteller.core.js
@@ -2,6 +2,7 @@ storyPlugin = {
     
     init : function(){
         console.log("initializing plugin...");
+        console.log("initialization in progress...");
         console.log("initialization done!");
     }
 
