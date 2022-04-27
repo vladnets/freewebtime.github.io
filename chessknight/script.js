5646ad7300307a08445ba6a1083d79a8ba04df62
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2022-04-27 19:56:40 +0300

    test changes

--- chessknight/script.js
+++ chessknight/script.js
@@ -1,126 +1,205 @@
 "use strict";
-var ChessKnight;
-(function (ChessKnight) {
-    var pathPrefix = "resources/WebGL/0.10/";
-    var buildUrl = pathPrefix + "Build/";
-    var loaderUrl = buildUrl + "ChessKnight.loader.js";
-    var config = {
-        dataUrl: buildUrl + "ChessKnight.data.unityweb",
-        frameworkUrl: buildUrl + "ChessKnight.framework.js.unityweb",
-        codeUrl: buildUrl + "ChessKnight.wasm.unityweb",
-        streamingAssetsUrl: "StreamingAssets",
-        companyName: "Freewebtime",
-        productName: "ChessKnight",
-        productVersion: "1.1.0.10",
-        // By default Unity keeps WebGL canvas render target size matched with
-        // the DOM size of the canvas element (scaled by window.devicePixelRatio)
-        // Set this to false if you want to decouple this synchronization from
-        // happening inside the engine, and you would instead like to size up
-        // the canvas DOM size and WebGL render target sizes yourself.
-        matchWebGLToCanvasSize: false
+var VladnetsLanding;
+(function (VladnetsLanding) {
+    let Orientation;
+    (function (Orientation) {
+        Orientation["Unknown"] = "orientation-unknown";
+        Orientation["Portrait"] = "orientation-portrait";
+        Orientation["Landscape"] = "orientation-landscape";
+        Orientation["Square"] = "orientation-square";
+    })(Orientation || (Orientation = {}));
+    const viewport = {
+        orientation: Orientation.Unknown,
+        width: 0,
+        height: 0
     };
-    var canvasElementQuery = "#player-canvas";
-    var unityPlayerLoading;
-    function showLoadingBar() {
-        var loadingBar = document.querySelector("#unity-loading-bar");
-        loadingBar.style.display = "block";
-    }
-    function hideLoadingBar() {
-        var loadingBar = document.querySelector("#unity-loading-bar");
-        loadingBar.style.display = "none";
-    }
-    function showMobileWarning() {
-        var mobileWarning = document.querySelector("#unity-mobile-warning");
-        mobileWarning.style.display = "block";
-        setTimeout(() => {
-            mobileWarning.style.display = "none";
-        }, 5000);
-    }
-    function loadUnityPlayer() {
-        if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-            config.devicePixelRatio = 1;
-            // showMobileWarning();
+    const layout = {
+        isDirty: false,
+        lastWidthClassName: "",
+        lastHeightClassName: "",
+        lastOrientationClassName: ""
+    };
+    const config = {
+        widths: {
+            none: {
+                size: 0,
+                name: "none"
+            },
+            tiny: {
+                size: 320,
+                name: "tiny"
+            },
+            small: {
+                size: 600,
+                name: "small"
+            },
+            medium: {
+                size: 800,
+                name: "medium"
+            },
+            normal: {
+                size: 1200,
+                name: "normal"
+            },
+            large: {
+                size: 1920,
+                name: "large"
+            },
+            huge: {
+                size: 0,
+                name: "huge"
+            }
+        },
+        heights: {
+            none: {
+                size: 0,
+                name: "none"
+            },
+            tiny: {
+                size: 320,
+                name: "tiny"
+            },
+            small: {
+                size: 480,
+                name: "small"
+            },
+            medium: {
+                size: 600,
+                name: "medium"
+            },
+            normal: {
+                size: 1000,
+                name: "normal"
+            },
+            large: {
+                size: 1280,
+                name: "large"
+            },
+            huge: {
+                size: 0,
+                name: "huge"
+            }
         }
-        unityPlayerLoading = new Promise((resolve, reject) => {
-            var script = document.createElement("script");
-            script.src = loaderUrl;
-            script.onload = () => {
-                resolve();
-            };
-            script.onerror = (error) => {
-                reject(error);
-            };
-            document.body.appendChild(script);
-        });
+    };
+    function updateViewport() {
+        var width = window.visualViewport.width * window.visualViewport.scale;
+        var height = window.visualViewport.height * window.visualViewport.scale;
+        var orientation = Orientation.Unknown;
+        if (width == height) {
+            orientation = Orientation.Square;
+        }
+        else if (width < height) {
+            orientation = Orientation.Portrait;
+        }
+        else {
+            orientation = Orientation.Landscape;
+        }
+        // nothing changed
+        // if (viewport.width === width && viewport.height === height && viewport.orientation === orientation) {
+        //   return;
+        // }
+        layout.isDirty = true;
+        viewport.width = width;
+        viewport.height = height;
+        viewport.orientation = orientation;
+        console.log("viewport", window.visualViewport.width * window.visualViewport.scale);
     }
-    ChessKnight.loadUnityPlayer = loadUnityPlayer;
-    function startUnityPlayer() {
-        if (unityPlayerLoading === null) {
+    VladnetsLanding.updateViewport = updateViewport;
+    function updateLayoutSize() {
+        const body = document.body;
+        if (body === null) {
             return;
         }
-        unityPlayerLoading
-            .then(() => {
-            var canvas = document.querySelector(canvasElementQuery);
-            var progressBarFull = document.querySelector("#unity-progress-bar-full");
-            var fullscreenButton = document.querySelector("#unity-fullscreen-button");
-            if (canvas === null) {
-                console.error("ERROR: Canvas is null");
-                return;
+        const { widths, heights } = config;
+        let widthClassName = "width-";
+        if (viewport.width < widths.tiny.size) {
+            widthClassName += widths.none.name;
+        }
+        else if (viewport.width < widths.small.size) {
+            widthClassName += widths.tiny.name;
+        }
+        else if (viewport.width < widths.medium.size) {
+            widthClassName += widths.small.name;
+        }
+        else if (viewport.width < widths.normal.size) {
+            console.log("really?", viewport.width);
+            widthClassName += widths.medium.name;
+        }
+        else if (viewport.width < widths.large.size) {
+            widthClassName += widths.normal.name;
+        }
+        else if (viewport.width > widths.large.size) {
+            widthClassName += widths.huge.name;
+        }
+        let heightClassName = "height-";
+        if (viewport.height < heights.tiny.size) {
+            heightClassName += heights.none.name;
+        }
+        else if (viewport.height < heights.small.size) {
+            heightClassName += heights.tiny.name;
+        }
+        else if (viewport.height < heights.medium.size) {
+            heightClassName += heights.small.name;
+        }
+        else if (viewport.height < heights.normal.size) {
+            heightClassName += heights.medium.name;
+        }
+        else if (viewport.height < heights.large.size) {
+            heightClassName += heights.normal.name;
+        }
+        else if (viewport.height > heights.large.size) {
+            heightClassName += heights.huge.name;
+        }
+        if (widthClassName !== layout.lastWidthClassName) {
+            if (layout.lastWidthClassName) {
+                body.classList.remove(layout.lastWidthClassName);
             }
-            if (canvas === null) {
-                console.error("ERROR: Canvas is null");
-                return;
+            body.classList.add(widthClassName);
+            layout.lastWidthClassName = widthClassName;
+        }
+        if (heightClassName !== layout.lastHeightClassName) {
+            if (layout.lastHeightClassName) {
+                body.classList.remove(layout.lastHeightClassName);
             }
-            var timeoutId;
-            var updateLayout = () => {
-                if (canvas === null) {
-                    return;
-                }
-                canvas.style.display = "none";
-                canvas.hidden = true;
-                clearTimeout(timeoutId);
-                timeoutId = setTimeout(() => {
-                    var canvasContainer = document.querySelector("#player-canvas-container");
-                    if (canvasContainer !== null) {
-                        canvas.width = canvasContainer.offsetWidth;
-                        canvas.height = canvasContainer.offsetHeight;
-                    }
-                    canvas.style.display = "";
-                    canvas.hidden = false;
-                }, 100);
-            };
-            document.addEventListener("fullscreenchange", updateLayout);
-            window.addEventListener("resize", updateLayout);
-            // updateLayout();
-            window.createUnityInstance(canvas, config, (progress) => {
-                if (progressBarFull === null) {
-                    return;
-                }
-                // updateLayout();
-                progressBarFull.style.width = 100 * progress + "%";
-            }).then((unityInstance) => {
-                // hideLoadingBar();
-                updateLayout();
-                if (fullscreenButton !== null) {
-                    fullscreenButton.onclick = () => {
-                        unityInstance.SetFullscreen(1);
-                    };
-                }
-                else {
-                    console.error("ERROR: no fullscreen button");
-                }
-            }).catch((message) => {
-                alert(message);
-            });
-        })
-            .catch((error) => {
-            console.log(`error! ${error.message}`);
-        });
+            body.classList.add(heightClassName);
+            layout.lastHeightClassName = heightClassName;
+        }
     }
-    ChessKnight.startUnityPlayer = startUnityPlayer;
-})(ChessKnight || (ChessKnight = {}));
-/// <reference path="./player.ts" />
-window.addEventListener("load", () => {
-    ChessKnight.loadUnityPlayer();
-    ChessKnight.startUnityPlayer();
-});
+    function updateLayout() {
+        // layout wasn't changed since last time
+        if (!layout.isDirty) {
+            return;
+        }
+        const body = document.body;
+        if (body !== null) {
+            if (viewport.orientation !== layout.lastOrientationClassName) {
+                const orientationClassName = viewport.orientation;
+                // remove last orientation class
+                if (layout.lastOrientationClassName) {
+                    body.classList.remove(layout.lastOrientationClassName);
+                }
+                // add orientation class
+                body.classList.add(orientationClassName);
+                // save last orientation to avoid unnecessary class update
+                layout.lastOrientationClassName = orientationClassName;
+            }
+        }
+        updateLayoutSize();
+        // reset layout dirtiness
+        layout.isDirty = false;
+    }
+    function onLoad() {
+        updateViewport();
+        updateLayout();
+    }
+    var resizeTimeoutId = -1;
+    function onResize() {
+        clearTimeout(resizeTimeoutId);
+        resizeTimeoutId = setTimeout(() => {
+            updateViewport();
+            updateLayout();
+        }, 1);
+    }
+    window.addEventListener("load", onLoad);
+    window.addEventListener("resize", onResize);
+})(VladnetsLanding || (VladnetsLanding = {}));
---
+++ chessknight/src/v3/scripts/index.ts
@@ -0,0 +1,230 @@
+namespace VladnetsLanding {
+  enum Orientation {
+    Unknown = "orientation-unknown",
+    Portrait = "orientation-portrait",
+    Landscape = "orientation-landscape",
+    Square = "orientation-square"
+  }
+
+  const viewport = {
+    orientation: Orientation.Unknown,
+    width: 0,
+    height: 0
+  };
+
+  const layout = {
+    isDirty: false,
+    lastWidthClassName: "",
+    lastHeightClassName: "",
+    lastOrientationClassName: ""
+  };
+
+  const config = {
+    widths: {
+      none: {
+        size: 0,
+        name: "none"
+      },
+      tiny: {
+        size: 320,
+        name: "tiny"
+      },
+      small: {
+        size: 600,
+        name: "small"
+      },
+      medium: {
+        size: 800,
+        name: "medium"
+      },
+      normal: {
+        size: 1200,
+        name: "normal"
+      },
+      large: {
+        size: 1920,
+        name: "large"
+      },
+      huge: {
+        size: 0,
+        name: "huge"
+      }
+    },
+    heights: {
+      none: {
+        size: 0,
+        name: "none"
+      },
+      tiny: {
+        size: 320,
+        name: "tiny"
+      },
+      small: {
+        size: 480,
+        name: "small"
+      },
+      medium: {
+        size: 600,
+        name: "medium"
+      },
+      normal: {
+        size: 1000,
+        name: "normal"
+      },
+      large: {
+        size: 1280,
+        name: "large"
+      },
+      huge: {
+        size: 0,
+        name: "huge"
+      }
+    }
+  };
+
+  export function updateViewport() {
+    var width = window.visualViewport.width * window.visualViewport.scale;
+    var height = window.visualViewport.height * window.visualViewport.scale;
+    var orientation: Orientation = Orientation.Unknown;
+
+    if (width == height) {
+      orientation = Orientation.Square;
+    }
+    else if(width < height) {
+      orientation = Orientation.Portrait;
+    }
+    else {
+      orientation = Orientation.Landscape;
+    }
+
+    // nothing changed
+    // if (viewport.width === width && viewport.height === height && viewport.orientation === orientation) {
+    //   return;
+    // }
+
+    layout.isDirty = true;
+    viewport.width = width;
+    viewport.height = height;
+    viewport.orientation = orientation;
+
+    console.log("viewport", window.visualViewport.width * window.visualViewport.scale);
+  }
+
+  function updateLayoutSize() {
+    const body = document.body;
+
+    if (body === null) {
+      return;
+    }
+
+    const { widths, heights } = config;
+
+    let widthClassName = "width-";
+
+    if (viewport.width < widths.tiny.size) {
+      widthClassName += widths.none.name;
+    }
+    else if (viewport.width < widths.small.size) {
+      widthClassName += widths.tiny.name;
+    }
+    else if (viewport.width < widths.medium.size) {
+      widthClassName += widths.small.name;
+    }
+    else if (viewport.width < widths.normal.size) {
+      console.log("really?", viewport.width);
+      widthClassName += widths.medium.name;
+    }
+    else if (viewport.width < widths.large.size) {
+      widthClassName += widths.normal.name;
+    }
+    else if (viewport.width > widths.large.size) {
+      widthClassName += widths.huge.name;
+    }
+
+    let heightClassName = "height-";
+
+    if (viewport.height < heights.tiny.size) {
+      heightClassName += heights.none.name;
+    }
+    else if (viewport.height < heights.small.size) {
+      heightClassName += heights.tiny.name;
+    }
+    else if (viewport.height < heights.medium.size) {
+      heightClassName += heights.small.name;
+    }
+    else if (viewport.height < heights.normal.size) {
+      heightClassName += heights.medium.name;
+    }
+    else if (viewport.height < heights.large.size) {
+      heightClassName += heights.normal.name;
+    }
+    else if (viewport.height > heights.large.size) {
+      heightClassName += heights.huge.name;
+    }
+
+    if (widthClassName !== layout.lastWidthClassName) {
+      if (layout.lastWidthClassName) {
+        body.classList.remove(layout.lastWidthClassName);
+      }
+
+      body.classList.add(widthClassName);
+      layout.lastWidthClassName = widthClassName;
+    }
+
+    if (heightClassName !== layout.lastHeightClassName) {
+      if (layout.lastHeightClassName) {
+        body.classList.remove(layout.lastHeightClassName);
+      }
+
+      body.classList.add(heightClassName);
+      layout.lastHeightClassName = heightClassName;
+    }
+  }
+
+  function updateLayout() {
+    // layout wasn't changed since last time
+    if (!layout.isDirty) {
+      return;
+    }
+
+    const body = document.body;
+
+    if (body !== null) {
+      if (viewport.orientation !== layout.lastOrientationClassName) {
+        const orientationClassName = viewport.orientation;
+        // remove last orientation class
+        if (layout.lastOrientationClassName) {
+          body.classList.remove(layout.lastOrientationClassName);
+        }
+
+        // add orientation class
+        body.classList.add(orientationClassName);
+        // save last orientation to avoid unnecessary class update
+        layout.lastOrientationClassName = orientationClassName;
+      }
+    }
+
+    updateLayoutSize();
+
+    // reset layout dirtiness
+    layout.isDirty = false;
+  }
+
+  function onLoad() {
+    updateViewport();
+    updateLayout();
+  }
+
+  var resizeTimeoutId = -1;
+  function onResize() {
+    clearTimeout(resizeTimeoutId);
+
+    resizeTimeoutId = setTimeout(() => {
+      updateViewport();
+      updateLayout();
+    }, 1);
+  }
+
+  window.addEventListener("load", onLoad)
+  window.addEventListener("resize", onResize);
+}
